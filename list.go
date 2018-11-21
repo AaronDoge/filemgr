81a27249116ab949ca9a9024b0eaf7277a187421
Aaron Chen <chendai2004@hotmail.com>
2018-11-21 20:17:53 +0800

    update
    Signed-off-by: Aaron Chen <chendai2004@hotmail.com>

--- list.go
+++ list.go
@@ -65,3 +65,42 @@ func (c *Client) List(files ...string) (flist []FileInfo, err error) {
 
 	return flist,nil
 }
+
+
+//获取指定命名空间的文件名（或者详细）列表
+func (c *Client) ListAll() (flist []FileInfo, err error) {
+
+	req, err := ufsdk.NewFileRequest(c.Conf, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	log.Println("正在获取文件列表...")
+	list, err := req.PrefixFileList("", "", 1000)
+	if err != nil {
+		log.Println("获取文件列表失败，错误信息为：", err.Error())
+		return nil, err
+	}
+
+	var count int
+	for _, v := range list.DataSet {
+		fi := FileInfo{}
+		tmp := strings.Split(v.FileName, "@")
+		ns := tmp[0]
+
+		fi.NSpace = ns
+		fi.FName = tmp[len(tmp)-1]
+		fi.FSize = v.Size
+		fi.MTime =  time.Unix(int64(v.ModifyTime), 0).Format(base_format)
+		fmt.Printf("%s	%d	%s	%s\n", ns, fi.FSize, fi.MTime, fi.FName)
+
+		flist = append(flist, fi)
+		count++
+	}
+	fmt.Printf("total %d \n", count)
+
+	// log.Printf("获取文件列表返回的信息是：\n%s\n", flist)
+
+	return flist,nil
+}
